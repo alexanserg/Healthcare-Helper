fa9d5b019420bf9f141f8a002f61b34dd9a5d692
Alexander Sergejev <alexandersergejev@gmail.com>
2019-07-05 10:58:53 -0700

    continue learning betterdoctor api; begin building backend logic for betterdoctor

--- src/healthcare.js
+++ src/healthcare.js
@@ -2,7 +2,8 @@ export class HealthCare {
   getDoctorByName(city) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2cdda81a44d71b558f9b89e9fddc19a0
+`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
