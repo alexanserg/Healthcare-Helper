cc0c9f0861c23bdd1ee51efd56417f89d187bc37
Alexander Sergejev <alexandersergejev@gmail.com>
2019-07-05 10:37:26 -0700

    spend time to get familiar with betterdoctor api

---
+++ src/healthcare.js
@@ -0,0 +1,32 @@
+export class HealthCare {
+  getDoctorByName(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  getDoctorBySymp(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,5 +4,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
-  console.log("hello");
-})
+  $('#weatherLocation').click(function() {
+   let city = $('#location').val();
+   $('#location').val("");
+
+   let promise = // code moved to _weather-service.js_
+
+   promise.then(function(response) {
+     let body = JSON.parse(response);
+     $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+     $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+   }, function(error) {
+     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+   });
+ });
+
+});
