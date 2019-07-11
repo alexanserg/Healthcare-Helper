c00639e19c6082f30d44ca74f3a24b02625586ff
Alexander Sergejev <alexandersergejev@gmail.com>
2019-07-10 22:00:26 -0700

    continue attempting to fix api call

--- src/main.js
+++ src/main.js
@@ -9,22 +9,20 @@ $(document).ready(function(){
     let first_name = $('#first_name').val();
     let last_name = $('#last_name').val();
     let symptom = $('#symptom').val();
-    let healthCare = new HealthCare();
     console.log(first_name);
     console.log(last_name);
     console.log(symptom);
-    let promise = healthCare.getDoctor(first_name,last_name,symptom);
+    let healthCare = new HealthCare();
+    let promise = healthCare.getDoctor();
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      console.log('hi')
-      $('.doctorInfo').text(body.data);
+      console.log(body.data);
+      body.data.forEach(function(doctor) {
+        $('.doctorInfo').append(`<li>${name}</li>`);
+      })
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
-  });
-
-});
-
-//
-// <br> ${body.data.profile.last_name} <br> ${body.data.profile.first_name} ${body.data.practices.visit_address.city}${body.data.practices.visit_address.street1}${body.data.practices.visit_address.street2}${body.data.practices.visit_address.zip}<br> ${body.data.practices.phones[0].number} ${body.data.practices.visit_address.city}
+  })
+})
