4fdca5dc5c888a2b87079bad7f7495199fcccbc7
Alexander Sergejev <alexandersergejev@gmail.com>
2019-07-12 20:45:30 -0700

    works for symptom

--- src/main.js
+++ src/main.js
@@ -13,14 +13,14 @@ $(document).ready(function(){
     console.log(last_name);
     console.log(symptom);
     let healthCare = new HealthCare();
-    let promise = healthCare.getDoctor();
+    let promise = healthCare.getDoctor(first_name, last_name, symptom);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body.data);
-      body.data.forEach(function(doctor) {
-        $('.doctorInfo').append(`<li>${name}</li>`);
-      })
+      console.log();
+      // body.data.forEach(function(doctor) {
+        $('.doctorInfo').html(`<li>${body.data[0].practices[0].name}</li><br><li>${body.data[0].practices[0].visit_address.street}</li><br><li>It is ${body.data[0].practices[0].accepts_new_patients} that this facility is accepting new patients</li><br><li>Phone Number: ${body.data[0].practices[0].phones[0].number}</li><br><li>Please visit ${body.data[0].practices[0].website}</li>`);
+      // })
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
